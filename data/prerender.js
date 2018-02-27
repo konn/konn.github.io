6edf01548cce9d21f52a35710c66a04275cac2c2
Hiromi ISHII <konn.jinro@gmail.com>
2018-02-27 15:43:33 +0900

    Error handling

--- data/prerender.js
+++ data/prerender.js
@@ -3,10 +3,14 @@ const {JSDOM} = jsdom;
 const dom = new JSDOM(require('fs').readFileSync('/dev/stdin', 'utf8'));
 document = dom.window.document;
 const renderMathInElement = require('auto-render');
-renderMathInElement(document.body, {
-    delimiters:
-    [ {left: "\\(", right: "\\)", display: false },
-      {left: "\\[", right: "\\]", display: true }
-    ]
-});
-process.stdout.write(dom.serialize());
+try {
+  renderMathInElement(document.body, {
+      delimiters:
+      [ {left: "\\(", right: "\\)", display: false },
+        {left: "\\[", right: "\\]", display: true }
+      ]
+  });
+  process.stdout.write(dom.serialize());
+} finally {
+  process.stdout.write(dom.serialize());
+}
